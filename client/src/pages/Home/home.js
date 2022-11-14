c0b90a961793fca9a12fa4cfa18ddc2b61065fec
ahmed <ahmedmuhammed78613@gmail.com>
2022-11-14 16:06:10 +0000

    idk

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 import { Button, Icon } from "semantic-ui-react";
 import "./home.css";
 
-export default function Home() {
+export default function Home({ setToggle }) {
 	return (
 		<div id="homepage">
 			<h1>MandemzMad</h1>
 			<div id="homeContent">
-				<NavLink to="/joinRoom">
+				<NavLink to="/room" onClick={() => setToggle("join")}>
 					<Button animated>
 						<Button.Content visible>Join Room</Button.Content>
 						<Button.Content hidden>
@@ -17,14 +17,14 @@ export default function Home() {
 					</Button>
 				</NavLink>
 
-				<NavLink to="/createRoom">
+				<NavLink to="/room" onClick={() => setToggle("create")}>
 					<Button animated="fade">
 						<Button.Content visible>Create Room</Button.Content>
 						<Button.Content hidden>Let's get started!</Button.Content>
 					</Button>
 				</NavLink>
 
-				<NavLink to="/leaderboards">
+				<NavLink to="/leaderboard">
 					<Button animated="vertical">
 						<Button.Content hidden>
 							<Icon name="trophy" />
--- client/src/pages/Leaderboard/Leaderboard.js
+++ client/src/pages/Leaderboard/Leaderboard.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import data from './data';
+import React from "react";
+import data from "./data";
 
-export default function LeaderBoard({ data }) {
-    return (
-        <div>
-            <h1>LeaderBoard</h1>
-            {data.map((user) => {
-                return <div>{user.name}</div>;
-            })}
-        </div>
-    );
+export default function LeaderBoard() {
+	return (
+		<div>
+			<h1>LeaderBoard</h1>
+			{data.map((user) => {
+				return <div>{user.name}</div>;
+			})}
+		</div>
+	);
 }

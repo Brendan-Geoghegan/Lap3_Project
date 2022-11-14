cdb304fb123eb20837e1bb0f1b90a8ea3e7a0f7d
ahmed <ahmedmuhammed78613@gmail.com>
2022-11-14 10:32:20 +0000

    createRoom component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CreateRoom from "./components/CreateRoom/CreateRoom";
 
 const App = () => {
 	return (
 		<div className="main-container">
 			<h1>App</h1>
+			<CreateRoom />
 		</div>
 	);
 };
--- client/src/components/CreateRoom/CreateRoom.js
+++ client/src/components/CreateRoom/CreateRoom.js
@@ -1,8 +1,58 @@
-import React from "react";
-import "./style.css";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3001");
 
 const CreateRoom = () => {
-	return <div>CreateRoom</div>;
+	const [room, setRoom] = useState("");
+	const [username, setUsername] = useState("");
+	const [allPlayers, setAllPlayers] = useState([]);
+
+	const joinRoom = () => {
+		// Sends message to Backend
+		if (room !== "" && username !== "") {
+			socket.emit("join_room", { room, username });
+		}
+	};
+
+	const leaveRoom = () => {
+		socket.emit("leave_room", { room, username });
+		window.location.reload();
+		// setRoom("");
+		// setUsername("");
+		// setAllPlayers([]);
+	};
+
+	useEffect(() => {
+		socket.off("update_room").on("update_room", (users) => {
+			setAllPlayers(users);
+		});
+	}, [socket]);
+	return (
+		<div>
+			{!allPlayers.length && (
+				<form onSubmit={joinRoom}>
+					<input
+						type="text"
+						placeholder="Room Code..."
+						onChange={(e) => setRoom(e.target.value)}
+					/>
+					<input
+						type="text"
+						placeholder="Username..."
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<input type="button" value="Join Room" onClick={joinRoom} />
+				</form>
+			)}
+			<h1>Players:</h1>
+			<div className="all-players">
+				{allPlayers.map((user, i) => (
+					<p key={i}>{user.username}</p>
+				))}
+			</div>
+			<button onClick={leaveRoom}>Leave</button>
+		</div>
+	);
 };
 
 export default CreateRoom;

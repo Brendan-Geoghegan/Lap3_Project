483f215e9e4169cc366413028c18a763b0cdce97
Brendan-Geoghegan <brendangeoghegan@outlook.com>
2022-11-14 10:20:51 +0000

    routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
+import { Home, Leaderboards, WaitingRoom, Quiz, Results, NotFound} from "./pages";
+
 function App() {
 	return (
-		<div className="App">
-			<h1>App</h1>
-		</div>
-	);
+		<>
+		<Routes>
+		<Route path="/" element={<Home/>} />
+		<Route path="/leaderboards" element={<Leaderboards/>} />
+		<Route path="/waitingRoom" element={<WaitingRoom/>} />
+		<Route path="/quiz" element={<Quiz/>} />
+		<Route path="/results" element={<Results/>} />
+		<Route path="*" element={<NotFound/>} />
+		</Routes>
+		</>
+	)
 }
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as WaitingRoom } from './WaitingRoom/WaitingRoom'
+export { default as Quiz } from './Quiz/Quiz'
+export { default as Results } from './Results/Results'
+export { default as NotFound } from './NotFound/NotFound'

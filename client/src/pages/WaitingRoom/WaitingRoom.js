1d7fc5bca9892a5f6fb5a2377a7fb8d909b9c5ce
Brendan-Geoghegan <brendangeoghegan@outlook.com>
2022-11-14 10:40:13 +0000

    Waiting room setup

--- client/src/pages/WaitingRoom/WaitingRoom.js
+++ client/src/pages/WaitingRoom/WaitingRoom.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
+const dummyData = {
+  code: "ABCD",
+  players: [
+    {
+      username: "Brendan" 
+    },
+    {
+      username: "Matt" 
+    },
+    {
+      username: "Ahmed" 
+    },
+    {
+      username: "Dave" 
+    }
+  ]
+}
+
 const WaitingRoom = () => {
   return (
-    <div>WaitingRoom</div>
+    <div>
+      <h1>Waiting for players...</h1>
+      <h2>Code: {dummyData.code}</h2>
+    </div>
   )
 }
 

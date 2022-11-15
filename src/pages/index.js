6aa271457df2ca6f9413dc7441391a889f512b8a
ahmed <ahmedmuhammed78613@gmail.com>
2022-11-15 15:58:07 +0000

    merge conflicts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-export { default as Leaderboard } from './Leaderboard/Leaderboard';
-export { default as Home } from './Home/home';
-export { default as WaitingRoom } from './WaitingRoom/WaitingRoom';
-export { default as Quiz } from './Quiz/Quiz';
-export { default as Results } from './Results/Results';
-export { default as NotFound } from './NotFound/NotFound';
+export { default as Leaderboard } from "./Leaderboard/Leaderboard";
+export { default as Home } from "./Home/Home";
+export { default as WaitingRoom } from "./WaitingRoom/WaitingRoom";
+export { default as Quiz } from "./Quiz/Quiz";
+export { default as Results } from "./Results/Results";
+export { default as NotFound } from "./NotFound/NotFound";

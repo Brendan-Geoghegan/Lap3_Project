39504e4847beeadba30cdee08f48126cb0899f5f
Matthieu Chan <matthieuchan96@gmail.com>
2022-11-15 09:29:24 +0100

    styling and structure continued

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as Leaderboard } from './Leaderboard/Leaderboard';
-export { default as Home } from './Home/home';
+export { default as Home } from './Home/Home';
 export { default as WaitingRoom } from './WaitingRoom/WaitingRoom';
 export { default as Quiz } from './Quiz/Quiz';
 export { default as Results } from './Results/Results';

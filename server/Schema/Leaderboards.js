a3c6b496642dead3b2b5764e8bd55b171da35442
ahmed <ahmedmuhammed78613@gmail.com>
2022-11-15 14:12:41 +0000

    updated file structure and removed backend

--- server/Schema/Leaderboards.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const leaderboardsSchema = mongoose.Schema({
-	username: { type: String, required: true },
-	score: { type: String || Number, required: true },
-});
-
-const leaderboardsModel = mongoose.model("leaderboards", leaderboardsSchema);
-module.exports = leaderboardsModel;

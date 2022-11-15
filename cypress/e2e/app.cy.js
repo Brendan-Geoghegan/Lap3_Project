9bab7c1e6e43e95a8f2103fb5a559cc8151140b9
David <siddav83@gmail.com>
2022-11-15 21:14:44 +0000

    added more tests

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,140 +1,141 @@
-describe("user creating a room", () => {
+// describe('user creating a room', () => {
+//     it('opens homepage successfully', () => {
+//         cy.visit('http://localhost:3000/');
+//     });
 
-  it("opens homepage successfully", () => {
-      cy.visit("http://localhost:3000/");
-  });
+//     it('user opens create room', () => {
+//         cy.get(':nth-child(2) > .ui').click();
+//         cy.get('[type="text"]').clear().type('mc');
+//         cy.get('[name="category"]').select('Animals');
+//         cy.get('[name="difficulty"]').select('Medium');
+//         cy.get('[type="submit"]').click();
+//     });
 
-    it("user opens create room", () => {
-      cy.get(':nth-child(2) > .ui').click();
-      cy.get('[type="text"]').clear().type('mc');
-      cy.get('[name="category"]').select('Animals');
-      cy.get('[name="difficulty"]').select('Medium');
-      cy.get('[type="submit"]').click();
-  });
+//     it('returns to the home page', () => {
+//         cy.get('.back-btn').click();
+//         cy.wait(500);
+//         cy.get('.back-btn').click();
+//     });
+// });
 
-    it("returns to the home page", () => {
-      cy.get('.back-btn').click();
-      cy.wait(500)
-      cy.get('.back-btn').click();
-    })
-});
+// describe('user joining a room', () => {
+//     it('opens homepage successfully', () => {
+//         cy.visit('http://localhost:3000/');
+//     });
 
-describe("user joining a room", () => {
+//     it('successfully opens join room', () => {
+//         cy.get(':nth-child(1) > .ui').click();
+//         cy.get('[placeholder="Enter Room..."]').type('1');
+//         cy.get('[placeholder="Enter Username..."]').type('Dave');
+//         cy.get('[type="submit"]').click();
+//     });
 
-  it("opens homepage successfully", () => {
-      cy.visit("http://localhost:3000/");
-  });
+//     it('returns to the home page', () => {
+//         cy.get('.back-btn').click();
+//         cy.wait(500);
+//         cy.get('.back-btn').click();
+//     });
+// });
 
-    it("successfully opens join room", () => {
-      cy.get(':nth-child(1) > .ui').click();
-      cy.get('[placeholder="Enter Room..."]').type('1')
-      cy.get('[placeholder="Enter Username..."]').type('Dave');
-      cy.get('[type="submit"]').click();
-  });
+// describe('user accesses leaderboard', () => {
+//     it('successfully opens leaderboard', () => {
+//         cy.get('[href="/leaderboard"] > .ui').click();
+//         cy.get('.leaderboard');
+//     });
 
-  it("returns to the home page", () => {
-    cy.get('.back-btn').click();
-      cy.wait(500)
-      cy.get('.back-btn').click();
-  })
-});
+//     it('returns to the home page', () => {
+//         cy.get('.back-btn').click();
+//     });
+// });
 
-describe("user accesses leaderboard", () => {
-  
-  it("successfully opens leaderboard", () => {
-    cy.get('[href="/leaderboard"] > .ui').click();
-    cy.get('.leaderboard');
+describe('multiplayer function', () => {
+    it('opens homepage successfully', () => {
+        cy.visit('http://localhost:3000/');
+    });
+
+    it('user opens create room', () => {
+        cy.get(':nth-child(2) > .ui').click();
+        cy.get('[type="text"]').clear().type('dave');
+        cy.get('[name="category"]').select('Animals');
+        cy.get('[name="difficulty"]').select('Medium');
+        cy.get('[type="submit"]').click();
+        // });
+    });
+
+    it('returns to the home page', () => {
+        cy.get('.back-btn').click();
+        cy.wait(500);
+    });
+
+    it('grab random room number', () => {
+        cy.get('h2').then(($num) => {
+            // cy.get('.back-btn').click();
         });
-  
-  it("returns to the home page", () => {
-          cy.get('.back-btn').click();
-        })
-})
+        const h2 = cy.get('h2');
+        console.log(h2);
+    });
+    //     it('successfully opens join room', () => {
+    //         cy.get(':nth-child(1) > .ui').click();
+    //         cy.get('[placeholder="Enter Room..."]').type('1');
+    //         cy.get('[placeholder="Enter Username..."]').type('Dave');
+    //         cy.get('[type="submit"]').click();
+    // });
+    // });
 
-describe("multiplayer function", () => {
+    //   it("Go Back To Login", () => {
+    //       cy.get(".login-btn").click();
+    //   });
 
-  it("opens homepage successfully", () => {
-      cy.visit("http://localhost:3000/");
-  });
+    //   it("Unsuccessful Login Attempt", () => {
+    //       cy.visit("https://monasteri.netlify.app/index.html");
+    //       cy.get("#username").type("wrong username");
+    //       cy.get("#password").type("wrong password");
+    //       cy.get("#login-form > .submit-btn").click();
+    //       cy.get(".fa-regular").click();
+    //   });
 
-    it("user opens create room", () => {
-      cy.get(':nth-child(2) > .ui').click();
-      cy.get('[type="text"]').clear().type('');
-      cy.get('[name="category"]').select('Animals');
-      cy.get('[name="difficulty"]').select('Medium');
-      cy.get('[type="submit"]').click();
-  });
+    //   it("Login Successfully", () => {
+    //       cy.get("#username").clear().type("testUser123");
+    //       cy.get("#password").clear().type("password");
+    //       cy.get("#login-form > .submit-btn").click();
+    //   });
 
-    it("returns to the home page", () => {
-      cy.get('.back-btn').click();
-      cy.wait(500)
-      cy.get('.back-btn').click();
-    })
+    //   it("Open Leaderboards", () => {
+    //       cy.get("#Leaderboard-btn").click();
+    //       cy.get("#go-back-btn").click();
+    //   });
 
-    it("successfully opens join room", () => {
-      cy.get(':nth-child(1) > .ui').click();
-      cy.get('[placeholder="Enter Room..."]').type('1')
-      cy.get('[placeholder="Enter Username..."]').type('Dave');
-      cy.get('[type="submit"]').click();
-  });
+    //   it("View a Habit", () => {
+    //       cy.get('[habit-id="635b84f8399f8f1d6ffe1804"]').click();
+    //   });
+
+    //   it("Increment Current on a habit", () => {
+    //       cy.get(".increment-habit-btn").click();
+    //       cy.wait(200);
+    //       cy.get(".back-btn").click();
+    //   });
+
+    //   it("Post new habit", () => {
+    //       cy.wait(200);
+    //       cy.get(".fa-solid").click();
+    //       cy.get("#habitName").type("Read");
+    //       cy.get("#habitTarget").type("2");
+    //       cy.get("#add-habit").click();
+    //   });
+
+    //   it("Open new habit", () => {
+    //       cy.wait(200);
+    //       cy.get(".habit-item").last().click();
+    //   });
+
+    //   it("Increment & remove", () => {
+    //       cy.wait(200);
+    //       cy.get(".increment-habit-btn").click();
+    //       cy.wait(200);
+    //       cy.get(".remove-habit-btn").click();
+    //   });
+
+    //   it("Logout", () => {
+    //       cy.wait(100);
+    //       cy.get("#logout-btn").click();
 });
-
-
-
-//   it("Go Back To Login", () => {
-//       cy.get(".login-btn").click();
-//   });
-
-//   it("Unsuccessful Login Attempt", () => {
-//       cy.visit("https://monasteri.netlify.app/index.html");
-//       cy.get("#username").type("wrong username");
-//       cy.get("#password").type("wrong password");
-//       cy.get("#login-form > .submit-btn").click();
-//       cy.get(".fa-regular").click();
-//   });
-
-//   it("Login Successfully", () => {
-//       cy.get("#username").clear().type("testUser123");
-//       cy.get("#password").clear().type("password");
-//       cy.get("#login-form > .submit-btn").click();
-//   });
-
-//   it("Open Leaderboards", () => {
-//       cy.get("#Leaderboard-btn").click();
-//       cy.get("#go-back-btn").click();
-//   });
-
-//   it("View a Habit", () => {
-//       cy.get('[habit-id="635b84f8399f8f1d6ffe1804"]').click();
-//   });
-
-//   it("Increment Current on a habit", () => {
-//       cy.get(".increment-habit-btn").click();
-//       cy.wait(200);
-//       cy.get(".back-btn").click();
-//   });
-
-//   it("Post new habit", () => {
-//       cy.wait(200);
-//       cy.get(".fa-solid").click();
-//       cy.get("#habitName").type("Read");
-//       cy.get("#habitTarget").type("2");
-//       cy.get("#add-habit").click();
-//   });
-
-//   it("Open new habit", () => {
-//       cy.wait(200);
-//       cy.get(".habit-item").last().click();
-//   });
-
-//   it("Increment & remove", () => {
-//       cy.wait(200);
-//       cy.get(".increment-habit-btn").click();
-//       cy.wait(200);
-//       cy.get(".remove-habit-btn").click();
-//   });
-
-//   it("Logout", () => {
-//       cy.wait(100);
-//       cy.get("#logout-btn").click();
-//   });

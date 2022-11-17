d4cc5113e7657d7715f1db84e65dc1510a98925a
ahmed <ahmedmuhammed78613@gmail.com>
2022-11-17 12:11:22 +0000

    added donut animation

--- src/pages/Results/Results.js
+++ src/pages/Results/Results.js
@@ -8,9 +8,6 @@ import { Confetti } from "../../components";
 
 const Results = () => {
 	const { userData, socket, results } = useContext(QuizContext);
-	console.log(userData);
-	console.log(results);
-	console.log(results.filter((user) => user.username !== userData.username));
 
 	useEffect(() => {
 		const postData = async () => {
@@ -72,53 +69,6 @@ const Results = () => {
 				<Link to="/" className="home-btn">
 					Back To home
 				</Link>
-
-				{/* <div id="finalResultsection">
-				<section>
-					Player 1<Progress percent={40} inverted color="red" progress />
-				</section>
-				<section>
-					Player 2
-					<Progress percent={59} inverted color="orange" progress />
-				</section>
-				<section>
-					Player 3<Progress percent={13} inverted color="yellow" progress />
-				</section>
-				<section>
-					Player 4<Progress percent={37} inverted color="olive" progress />
-				</section>
-				<section>
-					Player 5<Progress percent={83} inverted color="green" progress />
-				</section>
-				<section>
-					Player 6<Progress percent={23} inverted color="teal" progress />
-				</section>
-				<section>
-					Player 7<Progress percent={85} inverted color="blue" progress />
-				</section>
-				<section>
-					Player 8<Progress percent={38} inverted color="violet" progress />
-				</section>
-				<section>
-					Player 9<Progress percent={47} inverted color="purple" progress />
-				</section>
-				<section>
-					Player 10
-					<Progress percent={29} inverted color="pink" progress />
-				</section>
-				<section>
-					Player 11
-					<Progress percent={68} inverted color="brown" progress />
-				</section>
-				<section>
-					Player 12
-					<Progress percent={36} inverted color="grey" progress />
-				</section>
-				<section>
-					Player 13
-					<Progress percent={72} inverted color="black" progress />
-				</section>
-			</div> */}
 			</div>
 		</>
 	);
